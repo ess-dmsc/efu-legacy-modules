a87400454f8db2c7261174896d15f1146a90880e
Morten Jagd Christensen <mortenjc@jcaps.com>
2022-09-20 16:13:50 +0200

    import adc_readout from event-formation-unit

---
+++ adc_readout/AdcDataProcessor.cpp
@@ -0,0 +1,11 @@
+/** Copyright (C) 2018 European Spallation Source ERIC */
+
+/** @file
+ *
+ *  \brief Utility code for processing ADC data.
+ */
+
+#include "AdcDataProcessor.h"
+
+AdcDataProcessor::AdcDataProcessor(std::shared_ptr<ProducerBase> Prod)
+    : ProducerPtr(std::move(Prod)) {}
